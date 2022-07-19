6e0ba47c7e5cc9e5c283223873c4e30461591699
Lukasz <53575849+Lukasz7h@users.noreply.github.com>
2022-07-19 12:45:30 +0200

    hide data about err

--- profil/php/showProfilPhoto.php
+++ profil/php/showProfilPhoto.php
@@ -20,7 +20,7 @@
 
             $photo = $row[0];
             if(!$photo) $photo = "user.png";
-            $content = base64_encode(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/forum/profil_photos/".$photo));
+            @$content = base64_encode(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/forum/profil_photos/".$photo));
 
             echo $content;
         }
